4c55a77f10a9dbe1e3fb011d3cc52198e62b49e3
NoCoder504W <NoCoder504W@users.noreply.github.com>
2025-11-18 17:16:49 +0000

    deploy: ea05eeefd5325d6581561ac320e793acc65898e3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2540208276"
+    serviceWorkerVersion: "1759879659"
   }
 });
